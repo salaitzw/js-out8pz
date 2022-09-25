27ea396c20a3161c476c2d132d80093e87b9c72a
salaitzw <44660574+salaitzw@users.noreply.github.com>
2022-09-25 10:44:26 +0630

    2 files modified

--- index.js
+++ index.js
@@ -2,26 +2,30 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app')
-const imgContainer = document.createElement('div')
-
+const appDiv = document.getElementById('app');
 appDiv.innerHTML = `
-<button class = "shuffleCards">Shuffle cards</button>
-<button class = "drawCard">draw card</button>
-`
-const shuffleCards = document.querySelector(".shuffleCards")
+<div class = "container">
+  <button class = "shuffleCards">Shuffle cards</button>
+  <div class="imgContainer"></div>
+  <button class = "drawCard">draw card</button>
+</div>
+`;
+const shuffleCards = document.querySelector('.shuffleCards')
 const drawCardButton = document.querySelector('.drawCard')
+const imgContainer = document.querySelector('.imgContainer')
 
-let deckId = "new"
+/*let deckId = 'new'
+i will comment the above variable because i dont want to request too many new id from the api server*/
+let deckId = "8102zucikz1r"
 const goShuffleCards = () => {
-  console.log("testing of deckId", deckId)
+  console.log('testing of deckId', deckId);
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/?remaining=true`)
-      .then(res => res.json())
-      .then(data => {
-          console.log(data)
-          deckId = data.deck_id
-      })
-}
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+    });
+};
 
 const goFetchCard = () => {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`, {
@@ -30,17 +34,17 @@ const goFetchCard = () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-
-      appDiv.appendChild(imgContainer);
-      imgContainer.innerHTML = data.cards
+      let imgData = data.cards
         .map(
           (card) =>
             `
-          <img src = '${card.images.svg}'>
+          <img src = '${card.image}'>
         `
         )
         .join('');
+      console.log(imgData);
+      imgContainer.innerHTML = imgData
     });
 };
-shuffleCards.addEventListener('click', goShuffleCards )
-drawCardButton.addEventListener('click', goFetchCard)
+shuffleCards.addEventListener('click', goShuffleCards);
+drawCardButton.addEventListener('click', goFetchCard);
